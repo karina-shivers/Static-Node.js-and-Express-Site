8913b0cacd3718232021e8900ec9bf5a1d1884b2
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-11-07 14:18:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
+const express = require("express")
+const { render } = require("pug")
+const data = require("./data.json")
 
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/projects/:id', (req, res, next) => {
+    let projectId = parseInt(req.params.id)
+    let project = data.projects.find(project => project.id === projectId)
+    if (project) {
+        res.render('project', project)
+    } else {
+        next()
+    }
+})
+app.get('/', (req, res) => {
+    res.render('index', data)
+})
+app.get('*', (req, res, next) => {
+    error = new Error("Page not found")
+    error.status = 404
+    console.log(error.status, error.message)
+    throw error
+})
+
+app.use((error, req, res, next) => {
+    if (!error.status) {
+        error.status = 500
+    }
+    if (!error.message) {
+        error.message = "Unknown Server Error"
+    }
+    console.log(error.status, error.message)
+    if (error.status === 404) {
+        res.status(404).render('page-not-found', {error})
+    }
+    else {
+        res.status(error.status).render('error', {error})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
